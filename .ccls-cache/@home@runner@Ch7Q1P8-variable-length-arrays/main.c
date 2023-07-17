5c2159a511e5d8cf4a82dad8f04f11d2d2d4454a
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-17 13:55:35 +0000

    completed program

--- .ccls-cache/@home@runner@Ch7Q1P8-variable-length-arrays/main.c
+++ .ccls-cache/@home@runner@Ch7Q1P8-variable-length-arrays/main.c
@@ -1,6 +1,36 @@
+// Generate Fibonacci numbers using variable length arrays
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int i, numFibs;
+
+  // Get number of fibonacci numbers
+  printf ("How many Fibonacci numbers do you want (between 1 and 75)? ");
+  scanf ("%d", &numFibs);
+
+  // Test input (1 - 75)
+  if (numFibs < 1 || numFibs > 75) {
+    printf ("Bad number, sorry!\n");
+    return 1;
+  }
+
+  unsigned long long int Fibonacci[numFibs];
+
+  // Define first two fibonacci numbers
+  Fibonacci[0] = 0;
+  Fibonacci[1] = 1;
+
+  // Generate fibonacci numbers up to numFibs
+  for (i = 2; i < numFibs; i++) 
+    Fibonacci[i] = Fibonacci[i-2] + Fibonacci[i-1];
+
+  // Print the fibonacci numbers up to numFibs
+  for (i = 0; i < numFibs; i++)
+    printf ("%llu  ", Fibonacci[i]);
+
+  printf ("\n");
+  
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Programming in C
+## Chapter 7
+### Question 1: Part 8
+
+This problem is from chapter 7 of the textbook, it is to implement program 7.8[^1] from the textbook.
+
+You may wish to run the program and compare its output.
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 118 - Program 7.8
